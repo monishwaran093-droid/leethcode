46e046e9f48a6b5bcf44eca1c20dd3be656e3b31
monishwaran093-droid <monishwaran093@gmail.com>
2026-06-25 10:05:42 +0530

    Time: 55 ms (76.84%), Space: 22.4 MB (7.34%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -10,6 +10,6 @@ class Solution:
                 if nums[l]==0:
                     z-=1
                 l+=1
-            if  z<=k:
-                m=max(m,i-l+1)
+           
+            m=max(m,i-l+1)
         return m
