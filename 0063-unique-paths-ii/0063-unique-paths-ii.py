5ecb334e616bf7b202b4eff34e22635d3ea5a989
monishwaran093-droid <monishwaran093@gmail.com>
2026-07-08 09:16:00 +0530

    Time: 1 ms (31.73%), Space: 19.4 MB (62.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,21 +3,14 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         dp = [[0] * n for _ in range(m)]
-        if grid[0][0] == 1:
-            return 0
         dp[0][0] = 1
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    dp[i][j] = 0
-                    continue
-                if i == 0 and j == 0:
-                    continue
-                up = 0
-                left = 0
-                if i > 0:
-                    up= dp[i-1][j]
-                if j > 0:
-                    left= dp[i][j-1]
-                dp[i][j] = up + left
+                    dp[i][j] = 0   
+                else:
+                    if i > 0:
+                        dp[i][j]+= dp[i-1][j]
+                    if j > 0:
+                        dp[i][j]+= dp[i][j-1]
         return dp[m-1][n-1]
